ecc232c6d4d4ee0200acea776cdff0b5388666a9
S4M73l09 <sam1999xxx@gmail.com>
2026-02-20 18:27:01 +0100

    Fixes Error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 
 const repository = process.env.GITHUB_REPOSITORY?.split('/')[1];
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const isUserOrOrgPagesRepo = Boolean(repository && repository.endsWith('.github.io'));
+const basePath =
+  isGithubActions && repository && !isUserOrOrgPagesRepo ? `/${repository}` : '/';
 
 export default defineConfig({
   site: 'https://s4m73l09.github.io',
-  base: isGithubActions && repository ? `/${repository}` : '/',
+  base: basePath,
   output: 'static'
 });
